1c6a37da8b8bc16b714c0479e24d8007296afb18
sergioaguilar13y19 <sergioaguilar13y19@gmail.com>
2023-11-30 12:47:57 -0600

    Add todo list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  //create a component with a title, input field, and 2 button
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <input type="text" placeholder="Enter todo"/>
+      <button>Add</button>
+      <button>Clear</button>
+    </div>
+  );
 }
 
 export default App;
